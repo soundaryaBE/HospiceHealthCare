1c5ca2469e7d6af84f2e89263d67e541214c2b6a
soundaryaBE <soundaryaraj62@gmail.com>
2024-04-28 10:02:08 +0530

    committed successfully

--- HospiceHealthCare/src/main/java/com/GenericUtilities/IpathConstant.java
+++
@@ -1,9 +0,0 @@
-package com.GenericUtilities;
-
-public interface IpathConstant {
-String FilePath=".\\src\\test\\resources\\Testdata.txt";
-String ExcelPath=".\\src\\test\\resources\\HospiceHealthCare.xlsx";
-String DBURL="jdbc:mysql://localhost:3306/hospicehealthcare";
-String DBUsername="root";
-String DBPassword="root";
-}
--- HospiceHealthCare/src/main/java/com/healthCare/hospiceHealthCare/objectRepository/ManageUsersPage.java
+++
@@ -1,5 +0,0 @@
-package com.healthCare.hospiceHealthCare.objectRepository;
-
-public class ManageUsersPage {
-
-}
